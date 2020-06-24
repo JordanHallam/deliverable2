82b07b51cb5792781318448e083eaef6b3271483
JordanHallam <jghals12@gmail.com>
2020-06-24 11:24:50 -0400

    Deliverable2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine();
+            string capinput = input.ToUpper();
+            char[] inputarray = capinput.ToCharArray();
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(capinput);
+            int Checksum = 0;
+            String message = "";
+            foreach (byte bob in asciiBytes)
+            {
+                Checksum = Checksum + bob;
+                message = message + (bob - 64)+"-";
+            }
+                Console.WriteLine("Message Checksum is: "+Checksum);
+            Console.WriteLine("Your code is: "+ message); 
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        }
+    }
+
+
